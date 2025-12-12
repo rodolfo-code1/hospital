071c55f5c858cd49cb4d3733f30c64ee5ff0fd99
rodolfo-code1 <rodolfo.gallegos.1994@gmail.com>
2025-12-12 16:49:46 -0300

    Replace api_obtener_notificaciones with api_notificaciones

--- app/urls.py
+++ app/urls.py
@@ -8,6 +8,6 @@ app_name = 'app'
 urlpatterns = [
     path('', views.home, name='home'),
     path('notificacion/leida/<int:pk>/', views.marcar_leida, name='marcar_leida'),
-    
-    path('api/notificaciones/', views.api_obtener_notificaciones, name='api_notificaciones'),
+    path('api/notificaciones/', views.api_notificaciones, name='api_notificaciones'),
+    path('ocultar/<int:pk>/', views.ocultar_notificacion, name='ocultar_notificacion'),
 ]
