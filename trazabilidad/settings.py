74bf26995277aa18dbeb942cb9fe081202ef6210
Lukas Martínez <lukasmartinezc@gmail.com>
2025-12-13 22:17:47 -0300

    fix alertas

--- trazabilidad/settings.py
+++ trazabilidad/settings.py
@@ -184,11 +184,11 @@ AUTH_USER_MODEL = 'usuarios.Usuario'
 
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
-    messages.DEBUG: 'alert-secondary',
-    messages.INFO: 'alert-info',
-    messages.SUCCESS: 'alert-success',
-    messages.WARNING: 'alert-warning',
-    messages.ERROR: 'alert-danger',
+    messages.DEBUG: 'secondary',
+    messages.INFO: 'info',
+    messages.SUCCESS: 'success',
+    messages.WARNING: 'warning',
+    messages.ERROR: 'danger',
 }
 
 
